19433c52579eaf9ec2a434ca377fb3298ec7717e
Tomas <ravetomas@gmail.com>
2024-12-12 22:35:08 +0100

    Value

---
+++ src/app/dashboard/pages/activities/activities.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ButtonComponent } from '../../../shared/button/button.component';
+
+@Component({
+  selector: 'app-activities',
+  standalone: true,
+  imports: [ButtonComponent],
+  templateUrl: './activities.component.html',
+  styles: ``
+})
+export class ActivitiesComponent {
+
+}
--- src/app/dashboard/pages/home/home.component.ts
+++ src/app/dashboard/pages/home/home.component.ts
@@ -1,6 +1,5 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { ButtonComponent } from '../../../shared/button/button.component';
 import { Router } from '@angular/router';
 import {  inject } from '@angular/core';
 import { UsHomeComponent } from '../us-home/us-home.component';
@@ -8,18 +7,21 @@ import { InfoHomeComponent } from "../info-home/info-home.component";
 import ContactComponent from '../contact/contact.component';
 import { ParallaxComponent } from '../parallax/parallax.component';
 import { FooterComponent } from '../../../shared/footer/footer.component';
+import { HeaderComponent } from '../header/header.component';
+import { ActivitiesComponent } from '../activities/activities.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     CommonModule,
-    ButtonComponent,
     UsHomeComponent,
+    HeaderComponent,
     InfoHomeComponent,
     ContactComponent,
     ParallaxComponent,
-    FooterComponent
+    FooterComponent,
+    ActivitiesComponent
 ],
   styleUrl: './home.component.css',
   templateUrl: './home.component.html',
