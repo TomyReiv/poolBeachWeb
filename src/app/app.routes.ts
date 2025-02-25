de7071b9496b28f1d27a42febf9c1bf0998e1742
Tomas <ravetomas@gmail.com>
2025-02-25 14:57:49 +0100

    Ultimos cambios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -25,7 +25,7 @@ export const routes: Routes = [
       },
       {
         path: '',
-        redirectTo: 'Home',
+        redirectTo: 'dashboard/Home',
         pathMatch: 'full',
       },
     ],
